960108e388aa8185b8c50e3d1c41c2e627f1285b
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-03-24 14:27:20 -0400

    attempts generating freebie table

--- lib/models.py
+++ lib/models.py
@@ -25,5 +25,20 @@ class Dev(Base):
     id = Column(Integer(), primary_key=True)
     name= Column(String())
 
+
     def __repr__(self):
         return f'<Dev {self.name}>'
+    
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key=True)
+    item_name= Column(String())
+    value = Column(Integer())
+    company_id = Column(Integer(), ForeignKey("company.id"))
+    dev_id = Column(Integer(), ForeignKey("dev.id"))
+
+    def __repr__(self):
+        return f'<Freebie {self.item_name}, Value {self.value}>'
+
